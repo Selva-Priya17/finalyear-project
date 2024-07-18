24d62f2e3f08195554b6f0f94320d208d42796d3
Selva-Priya17 <selvapriya0017@gmail.com>
2024-07-19 00:49:35 +0530

    Modified test.js

---
+++ finalyear-project/.github/01_basics/test.js
@@ -0,0 +1,11 @@
+//console.log("priya");
+/*prefer not to use var since the issues of block of scope and functional of scope where if you change var
+ varaiable of one will overwrite others also if let a; let varaible is not given any value you will get ouput as undefined */ 
+const myArr=new Array(0,1,2,3,4,5);
+const myAr=new Array(0,1,2,3,4,5);
+const s1=myAr.slice(1,3);
+const s2=myArr.splice(1,3);
+console.log(s1)//for slice 3 is not included whereas in splice 3 is included that is the last number given in the splice and slice range
+console.log(s2)//another difference is that
+console.log(myAr)//for slice it does not affect the original data and just displayed the number between the given range
+console.log(myArr)//where as splice removed the range of numbers that came in as output of splice from original data thus original data is affected
---
+++ finalyear-project/README.md
@@ -0,0 +1,3 @@
+# finalyear-project
+This is to work on final year project
+ 
