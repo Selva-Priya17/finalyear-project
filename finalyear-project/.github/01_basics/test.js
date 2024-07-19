49429d3983cfee514515579e70a2c0874c3e0538
Selva-Priya17 <selvapriya0017@gmail.com>
2024-07-19 22:53:07 +0530

    Test

--- finalyear-project/.github/01_basics/test.js
+++ finalyear-project/.github/01_basics/test.js
@@ -8,4 +8,5 @@ const s2=myArr.splice(1,3);
 console.log(s1)//for slice 3 is not included whereas in splice 3 is included that is the last number given in the splice and slice range
 console.log(s2)//another difference is that
 console.log(myAr)//for slice it does not affect the original data and just displayed the number between the given range
-console.log(myArr)//where as splice removed the range of numbers that came in as output of splice from original data thus original data is affected
+console.log(myArr)
+//where as splice removed the range of numbers that came in as output of splice from original data thus original data is affected
